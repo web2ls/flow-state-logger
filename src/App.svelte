ed732c2780d986c1ccb384e6aaec39873740de3a
Alexey Slobodyansky <web2ls@yandex.ru>
2022-01-27 23:03:05 +0300

    common changes

--- src/App.svelte
+++ src/App.svelte
@@ -26,24 +26,4 @@
 		width: 100%;
 		height: 100%;
 	}
-
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
